b6097eddd93ee2cc4577f54ae27627aa59899d2e
Laurent SIMON <simon.laurent208@gmail.com>
2023-09-20 17:12:56 +0200

    ...

--- src/index.js
+++ src/index.js
@@ -1,24 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
 import './index.css';
 import HomePage from './pages/homePage/HomePage';
 import reportWebVitals from './reportWebVitals';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <HomePage />,
-  }
-])
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-          <RouterProvider router={router} />
+          <HomePage />
   </React.StrictMode>
 );
 
